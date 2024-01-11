3a5d0c186595eac93bcf7f1cc5fe8a591c29d6a7
Dmitriy Kuptsov <dmitriy.kuptsov@gmail.com>
2024-01-11 23:16:20 +0500

    Added thread lock

--- controller/controller.py
+++ controller/controller.py
@@ -27,7 +27,7 @@ __status__ = "development"
 from config import config
 hip_config = config.config;
 
-from database.models import DevicesModel
+from database.models import DevicesModel, HashesModel
 import sqlalchemy as sa
 from sqlalchemy.orm import sessionmaker
 
@@ -48,6 +48,12 @@ engine = sa.create_engine(connection_url)
 Session = sessionmaker(bind=engine)
 Session.configure(bind=engine)
 session = Session()
+
+# Threading
+import threading 
+
+# Randome 
+from os import urandom
 
 # Timing
 from time import time
@@ -83,6 +89,7 @@ open_addresses = []
 
 # HIP controller lock
 hip_config_socket_lock = threading.Lock()
+db_lock = threading.Lock()
 
 # Configuration parameters
 hostname = hip_config["network"]["hostname"];
@@ -120,6 +127,7 @@ def accept_loop():
             hip_config_socket_lock.release();
 
 def receive_loop(socket_):
+    global db_lock
     buf = bytearray([])
     while True:
         buf += socket_.recv(8000)
@@ -188,33 +196,93 @@ def receive_loop(socket_):
         ip = packet.get_ip();
         timestamp = int(time());
         
+        db_lock.acquire()
         device = session.query(DevicesModel).filter_by(hit = misc.Utils.ipv6_bytes_to_hex_formatted(hit), ip = misc.Utils.ipv4_bytes_to_string(ip)).first()
         if not device:
             device = DevicesModel();
             device.hit = misc.Utils.ipv6_bytes_to_hex_formatted(hit)
             device.ip = misc.Utils.ipv4_bytes_to_string(ip)
             device.timestamp = timestamp
+            
             session.add(device)
             session.commit();
         else:
             device.timestamp = timestamp
             session.commit();
+        db_lock.release()
         print(hexlify(hit))
         print(hexlify(ip))
         print(timestamp);
 
 def send_loop():
+    global db_lock
     while True:
-        for i in range(0, len(open_sockets)):
-            sock = open_sockets[i]
-            addr = open_addresses[i]
-            # Send messages to each HIP switch
+        try:
+            for i in range(0, len(open_sockets)):
+                sock = open_sockets[i]
+                addr = open_addresses[i]
+                hosts = session.query(DevicesModel).all();
+                hosts_ = []
+                buf_ = bytearray([])
+                length = 0
+                num_hosts = 0
+                for h in hosts:
+                    hosts_.append({
+                        "hit": misc.Utils.ipv6_to_bytes(h.hit),
+                        "ip": misc.Utils.ipv4_to_bytes(h.ip)
+                    })
+                    buf_ += misc.Utils.ipv6_to_bytes(h.hit) + misc.Utils.ipv4_to_bytes(h.ip)
+                    length += 20
+                    num_hosts += 1
+                sha = digest.SHA256Digest()
+                packet_hash = hexlify(sha.digest(buf_)).decode("ascii")
+                print("-------------------------------------------------------")
+                db_lock.acquire()
+                print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+                device = session.query(DevicesModel).filter_by(ip = addr[0]).first()
+                hash = session.query(HashesModel).filter_by(device_id = device.id, type = "HOSTS").first()
+                
+                if not hash:
+                    hash = HashesModel()
+                    hash.device_id = device.id
+                    hash.type = "HOSTS"
+                    hash.hash = packet_hash
+                    session.add(hash)
+                    session.commit()
+                else:
+                    if packet_hash == hash.hash:
+                        db_lock.release()
+                        continue
+                    else:
+                        hash.hash = packet_hash
+                        session.commit()
+                db_lock.release()
+                packet = packets.HostsConfigurationPacket()
+                packet.set_nonce(urandom(4))
+                print("111111111111111111111111")
+                packet.set_hosts(hosts_, num_hosts)
+                print("222222222222222222222222")
+                packet.set_packet_type(packets.HOSTS_CONFIGURATION_TYPE)
+                print("333333333333333333333333")
+                packet.set_packet_length(packets.BASIC_HEADER_OFFSET + length)
+                print("444444444444444444444444")
+                buf_ = packet.get_buffer()
+                hmac = digest.SHA256HMAC(master_secret)
+                computed_hmac = hmac.digest(buf_)
+                print("555555555555555555555555")
+                packet.set_hmac(computed_hmac)
+                print("-------------------- Sending configuration packets to the host ------------------");
+                send_bytes = sock.send(packet.get_buffer())
+                print("SENT %d BYTES TO SWITCH" % send_bytes)
+                # Send messages to each HIP switch
+        except Exception as e:
+            print(e)
+            pass
         sleep(1)
 
 
 hip_th_loop = threading.Thread(target = accept_loop, args = (), daemon = True);
 hip_th_loop.start();
 
-while True:
-    # Maintainance loop
-    sleep(1)
+send_loop();
+
