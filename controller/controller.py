91ac7f60504512921aa1b19345642d601f3cf37e
Dmitriy Kuptsov <dmitriy.kuptsov@gmail.com>
2024-09-19 21:35:22 +0500

    change comapny name

--- controller/controller.py
+++ controller/controller.py
@@ -16,7 +16,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 __author__ = "Dmitriy Kuptsov"
-__copyright__ = "Copyright 2024, stangebit"
+__copyright__ = "Copyright 2024, strangebit"
 __license__ = "GPL"
 __version__ = "0.0.1a"
 __maintainer__ = "Dmitriy Kuptsov"
